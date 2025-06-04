8cceb8d53ffebe7565e28f99fa6cd360a383a78f
poldanf22 <poldanf22@gmail.com>
2025-06-04 13:28:18 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Memuat variabel lingkungan dari file .env
+load_dotenv()
+
+# Ambil kredensial dari variabel lingkungan
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+# Koneksi ke MySQL menggunakan kredensial dari .env
+conn = mysql.connector.connect(
+    host=db_host,      # Host dari variabel lingkungan
+    user=db_user,      # Username dari variabel lingkungan
+    password=db_password,  # Password dari variabel lingkungan
+    database=db_name   # Nama database dari variabel lingkungan
+)
+
+# Membuat cursor untuk mengeksekusi query
+cursor = conn.cursor()
+
+# Menjalankan query untuk mengambil data
+cursor.execute("SELECT * FROM tbl_akreditasi")
+rows = cursor.fetchall()
+
+# Menampilkan data yang diambil
+for row in rows:
+    print(row)
+
+# Menutup koneksi
+conn.close()
